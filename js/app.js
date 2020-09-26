9e0209b13b82f28e34b4b5f99fafb71180566ab2
Artur Alves Ribeiro <67174471+ArturARibeiro@users.noreply.github.com>
2020-09-26 14:15:49 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
 function Active(){
 
-    if(window.pageYOffset <= 700){
+    if(window.pageYOffset <= (document.getElementById('introducao').offsetTop - 300)){
         $('#homelink').addClass('active');
     }else{
         $('#homelink').removeClass('active');
     }
 
-    if(window.pageYOffset > 700){
+    if(window.pageYOffset > (document.getElementById('introducao').offsetTop - 300)){
         $('#introlink').addClass('active');
     }else{
         $('#introlink').removeClass('active');
     }
 
-    if(window.pageYOffset >= 160){
+    if(window.pageYOffset >= 200 && document.body.offsetWidth > 768){
         $('#menu').addClass('activeBar fixed-top');
-        // document.getElementById('home').style.marginTop= "13.5vh";
+        document.getElementById('home').style.marginTop= "13.5vh";
     }else{
         $('#menu').removeClass('activeBar fixed-top');
-        // document.getElementById('home').style.marginTop= "0vh";
+        document.getElementById('home').style.marginTop= "0vh";
     }
 
     console.log(window.pageYOffset)
