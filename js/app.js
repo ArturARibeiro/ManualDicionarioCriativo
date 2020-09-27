2365f9c8083c933aed7e293ad96add8d4041ff79
Artur Alves Ribeiro <67174471+ArturARibeiro@users.noreply.github.com>
2020-09-26 23:16:20 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,32 +1,39 @@
-$('nav a').click(function(e){
-	e.preventDefault();
-	var id = $(this).attr('href'),
-			menuHeight = $('nav').innerHeight(),
-			targetOffset = $(id).offset().top;
-	$('html, body').animate({
-		scrollTop: targetOffset - menuHeight - 50
-	}, 400);
+$('nav a').click(function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        menuHeight = $('nav').innerHeight(),
+        targetOffset = $(id).offset().top;
+    $('html, body').animate({
+        scrollTop: targetOffset - menuHeight - 50
+    }, 400);
 });
 
-function Active(){
+function Active() {
 
-    if(window.pageYOffset <= (document.getElementById('introducao').offsetTop - 300)){
+    if (window.pageYOffset <= (document.getElementById('introducao').offsetTop - 300)) {
         $('#homelink').addClass('active');
-    }else{
+    } else {
         $('#homelink').removeClass('active');
     }
 
-    if(window.pageYOffset > (document.getElementById('introducao').offsetTop - 300)){
+    if (window.pageYOffset > (document.getElementById('introducao').offsetTop - 300)
+        && window.pageYOffset < (document.getElementById('historia').offsetTop - 300)) {
         $('#introlink').addClass('active');
-    }else{
+    } else {
         $('#introlink').removeClass('active');
     }
 
-    if(window.pageYOffset >= 200 && document.body.offsetWidth > 768){
+    if (window.pageYOffset > (document.getElementById('historia').offsetTop - 300)) {
+        $('#histlink').addClass('active');
+    } else {
+        $('#histlink').removeClass('active');
+    }
+
+    if (window.pageYOffset >= 200 && document.body.offsetWidth > 768) {
         $('#menu').addClass('activeBar fixed-top');
-        document.getElementById('home').style.marginTop= "13.5vh";
-    }else{
+        document.getElementById('home').style.marginTop = "13.5vh";
+    } else {
         $('#menu').removeClass('activeBar fixed-top');
-        document.getElementById('home').style.marginTop= "0vh";
+        document.getElementById('home').style.marginTop = "0vh";
     }
 }
